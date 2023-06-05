2fc8df41b8ec4a5baa3b318fffeda691128b1f33
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2023-06-05 20:00:18 -0300

    order pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { isAuthenticadet } from "./middleware/isAuthenticadet";
 
 import uploadConfig from './config/multer';
@@ -31,10 +32,14 @@ router.post('/category', isAuthenticadet, new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticadet, new ListCategoryController().handle)
 
-//-ROTAS PRODUCT
+//-- ROTAS PRODUCT
 router.post('/product', isAuthenticadet, upload.single('file'), new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticadet, new ListByCategoryController().handle)
+
+//-- ROTAS ORDER
+
+router.post('/order', isAuthenticadet, new CreateOrderController().handle)
 
 export { router };
 
