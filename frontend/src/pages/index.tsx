af9cc6cba334333c06dfb9820f7c8f7007f275c3
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2023-06-16 18:49:39 -0300

    Criando um Document

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,15 @@
-
+import Head from "next/head";
 import React from "react";
 
 export default function Home() {
   return (
+      <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
       <div>
-        <h1>Sujeito Pizza </h1>
+        <h1>Sujeito Pizza</h1>
       </div>
+      </>
   )
 }
