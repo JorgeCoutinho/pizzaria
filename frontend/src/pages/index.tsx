aaba5e9453491e19f26f7a2e20693025dc87b7c1
JORGE MIKAEL COUTINHO ALVES <jmca.lic19@uea.edu.br>
2023-06-18 16:10:35 -0300

    Criando tela login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,33 @@
 import Head from "next/head";
 import React from "react";
+import style from '../../styles/home.module.scss'
+import logoImg from '../../public/logo.svg'
+import Image from "next/image";
+import { Input } from "../components/ui/input";
+
 
 export default function Home() {
   return (
-      <>
+    <>
       <Head>
         <title>SujeitoPizza - Faça seu login</title>
       </Head>
-      <div>
-        <h1>Sujeito Pizza</h1>
+      <div className={style.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
       </div>
-      </>
+
+      <div className={style.login} >
+        <form>
+          <Input
+            placeholder="Digite seu email"
+            type="text"
+          />
+          <Input
+            placeholder="Digite seu senha"
+            type="password"
+          />
+        </form>
+      </div>
+    </>
   )
 }
